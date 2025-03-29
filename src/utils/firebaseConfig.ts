0ce327bccf5adc1695d055bd9c480c150b838749
Aqsa Aqeel <aqsa.aqeel17@example.com>
2025-03-29 18:05:50 +0530

    feat: added env file

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTEh6ICGayIl7hA8qk3Rf6ws9dxYJflm0",
-  authDomain: "send-eidi-d61a8.firebaseapp.com",
-  projectId: "send-eidi-d61a8",
-  storageBucket: "send-eidi-d61a8.firebasestorage.app",
-  messagingSenderId: "510931867344",
-  appId: "1:510931867344:web:3c07bc63650f375be7f0d5",
-  measurementId: "G-7WBKYYG03N"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+  
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
